e4dde65b211821be594b29520c5971e5c6b035f1
国与同 <guoyutong@guoyutongdeMacBook-Pro.local>
2020-02-27 09:09:38 +0800

    添加注释

--- ring_queue.h
+++ ring_queue.h
@@ -1,3 +1,18 @@
+/*
+Copyright (c) 2005-2020, Fishily Guo
+Permission to use, copy, modify, and/or distribute this software
+for any purpose with or without fee is hereby granted, provided
+that the above copyright notice and this permission notice appear
+in all copies.
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
 #include <iostream>
 using namespace std;
 
@@ -32,7 +47,7 @@ public:
 	{
 		if (isFull())
 		{
-			throw "The queue is full£¬cannot keep pushing.";
+			throw "The queue is full, cannot keep pushing.";
 		}
 		m_data[m_rear] = data;
 		m_rear = (m_rear + 1) % m_size;
@@ -41,7 +56,7 @@ public:
 	{
 		if (isEmpty())
 		{
-			throw new exception("The queue is empty£¬cannot keep poping.");
+			throw "The queue is empty, cannot keep poping.";
 		}
 		m_front = (m_front + 1) % m_size;
 	}
@@ -49,7 +64,7 @@ public:
 	{
 		if (isEmpty())
 		{
-			throw new exception("The queue is empty£¬cannot keep poping.");
+			throw "The queue is empty, cannot keep poping.";
 		}
 		while (m_front != m_rear)
 			m_front = (m_front + 1) % m_size;
@@ -58,7 +73,7 @@ public:
 	{
 		if (isEmpty())
 		{
-			throw new exception("The queue is empty£¬cannot access the top element.");
+			throw "The queue is empty, cannot access the top element.";
 		}
 		return m_data[m_front];
 	}
